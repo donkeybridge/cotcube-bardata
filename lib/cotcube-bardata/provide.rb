b8e7c05faa3d61200b60fb8fe411eee3821bb9df
Benjamin L. Tischendorf <donkeybridge@jtown.eu>
2020-12-20 19:39:35 +0000

    moved daily stuff into new file, prepare .provide to become a major accessor method

--- lib/cotcube-bardata/provide.rb
+++ lib/cotcube-bardata/provide.rb
@@ -3,85 +3,21 @@
 module Cotcube
   module Bardata
 
-    def provide(symbol: nil, id: nil, contract:, config: init)
-      raise ArgumentError, "Contract '#{contract}' is bogus, should be like 'M21' or 'ESM21'" unless contract.is_a? String and [3,5].include? contract.size
-      if contract.to_s.size == 5
-        symbol   = contract[0..1]
-        contract = contract[2..4] 
+    def provide(symbol: nil, id: nil, contract:, config: init, date: Date.today - 1, type: nil, fill: :none)
+      case type
+      when :eod, :eods
+        provide_eods(symbol: symbol, id: id, contract: contract, date: date) 
+      when :quarters
+        print :quarters
+      when :hours
+        print :hours
+      when :daily, :dailies
+        print :dailies
+      else
+        puts "WARNING: Using provide without :type is for legacy support pointing to .provide_daily".light_yellow
+        provide_daily(symbol: symbol, id: id, contract: contract, config: config)
       end
-      unless symbol.nil?
-        symbol_id = symbols.select{|s| s[:symbol] == symbol.to_s.upcase}.first[:id]
-        raise ArgumentError, "Could not find match in #{config[:symbols_file]} for given symbol #{symbol}" if symbol_id.nil?
-        raise ArgumentError, "Mismatching symbol #{symbol} and given id #{id}" if not id.nil? and symbol_id != id
-        id = symbol_id
-      end
-      raise ArgumentError, "Could not guess :id or :symbol from 'contract: #{contract}', please clarify." if id.nil?
-      id_path   = "#{config[:data_path]}/daily/#{id}"
-      data_file = "#{id_path}/#{contract}.csv"
-      raise RuntimeError, "No data found for requested :id (#{id_path} does not exist)" unless Dir.exist?(id_path)
-      raise RuntimeError, "No data found for requested contract #{symbol}:#{contract} in #{id_path}." unless File.exist?(data_file)
-      data = CSV.read(data_file, headers: %i[contract date open high low close volume oi] ).map do |row|
-        row = row.to_h
-        row.each do |k, _| 
-          row[k] = row[k].to_f if [:open, :high, :low, :close].include? k
-          row[k] = row[k].to_i if [:volume, :oi].include? k
-        end
-        row
-      end
-      data
-    end
-
-    def continuous(symbol: nil, id: nil, config: init)
-      unless symbol.nil?
-        symbol_id = symbols.select{|s| s[:symbol] == symbol.to_s.upcase}.first[:id]
-        raise ArgumentError, "Could not find match in #{config[:symbols_file]} for given symbol #{symbol}" if symbol_id.nil?
-        raise ArgumentError, "Mismatching symbol #{symbol} and given id #{id}" if not id.nil? and symbol_id != id
-        id = symbol_id
-      end
-      raise ArgumentError, "Could not guess :id or :symbol, please clarify." if id.nil?
-      id_path    = "#{config[:data_path]}/daily/#{id}"
-      available_contracts = Dir[id_path + '/*.csv'].map{|x| x.split('/').last.split('.').first}.sort_by{|x| x[-7]}.sort_by{|x| x[-6..-5]}
-      data = [] 
-      available_contracts.each do |c| 
-        provide(id: id, config: config, contract: c).each do |x| 
-          data << x
-        end
-      end
-      result = [] 
-      data.sort_by{|x| x[:date]}.group_by{|x| x[:date]}.map{|k,v| 
-        v.map{|x| x.delete(:date)}
-        result << {
-          date: k,
-          volume: v.map{|x| x[:volume]}.reduce(:+),
-          oi:     v.map{|x| x[:oi    ]}.reduce(:+)
-        }
-        result.last[:contracts] = v
-      } 
-      result
-    end
-
-    def continuous_overview(symbol: nil, id: nil, config: init, selector: :volume, human: false, filter: nil)
-      raise ArgumentError, "Selector must be either :volume or :oi" unless selector.is_a? Symbol and [:volume, :oi].include? selector
-      
-      unless symbol.nil?
-        symbol_id = symbols.select{|s| s[:symbol] == symbol.to_s.upcase}.first[:id]
-        raise ArgumentError, "Could not find match in #{config[:symbols_file]} for given symbol #{symbol}" if symbol_id.nil?
-        raise ArgumentError, "Mismatching symbol #{symbol} and given id #{id}" if not id.nil? and symbol_id != id
-        id = symbol_id
-      end
-      raise ArgumentError, "Could not guess :id or :symbol, please clarify." if id.nil?
-      data = continuous(id: id, config: config).map{|x| 
-        {
-          date:   x[:date],
-          volume: x[:contracts].sort_by{|x| - x[:volume]}[0..4].compact.select{|x| not x[:volume].zero?},
-          oi:     x[:contracts].sort_by{|x| - x[:oi]}[0..4].compact.select{|x| not x[:oi].zero?}
-        }
-      }.select{|x| not x[selector].empty? }
-      result = data.group_by{|x| x[selector].first[:contract]}
-      if human
-        result.each {|k,v| puts "#{k}\t#{v.first[:date]}\t#{v.last[:date]}" if filter.nil? or v.first[selector].first[:contract][2..4]=~ /#{filter}/ }
-      end
-      result
     end
   end
+
 end
