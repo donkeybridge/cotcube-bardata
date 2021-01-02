54f3c543bb6628edf695de1158540a3c09c94c28
Benjamin L. Tischendorf <donkeybridge@jtown.eu>
2021-01-02 00:13:10 +0000

    eods.rb: renamed get_id_from to get_id_set
    eods.rb: applied cops, added explanations

--- lib/cotcube-bardata/eods.rb
+++ lib/cotcube-bardata/eods.rb
@@ -3,27 +3,20 @@
 module Cotcube
   # Missing top level documentation
   module Bardata
-    def get_id_from(symbol: nil, id: nil, config: init)
-      unless symbol.nil?
-        symbol_id = symbols.select { |s| s[:symbol] == symbol.to_s.upcase }.first[:id]
-        if symbol_id.nil?
-          raise ArgumentError,
-                "Could not find match in #{config[:symbols_file]} for given symbol #{symbol}"
-        end
-        raise ArgumentError, "Mismatching symbol #{symbol} and given id #{id}" if (not id.nil?) && (symbol_id != id)
-
-        id = symbol_id
-      end
-      raise ArgumentError, 'Need :id or :symbol.' if id.nil?
-    end
-
     def most_liquid_for(symbol: nil, id: nil, date: last_trade_date, config: init)
-      id = get_id_from(symbol: symbol, id: id, config: config)
+      id = get_id_set(symbol: symbol, id: id, config: config)[:id]
       provide_eods(id: id, dates: date, contracts_only: true).first
     end
 
-    def provide_most_liquids_by_eod(config: init, date: last_trade_date, filter: :volume_part, age: 1.hour)
-      eods = provide_eods(config: config, dates: date, filter: filter)
+    # the following method seems to be garbage. It is not used anywhere. It seems it's purpose
+    # was to retrieve a list of quarters that have not been fetched recently (--> :age)
+    def provide_most_liquids_by_eod(symbol: nil, id: nil, # rubocop:disable Metrics/ParameterLists
+                                    config: init,
+                                    date: last_trade_date,
+                                    filter: :volume_part,
+                                    age: 1.hour)
+      sym  = get_id_set(symbol: symbol, id: id) if symbol or id
+      eods = provide_eods(id: sym.nil? ? nil : sym[:id], config: config, dates: date, filter: filter)
       result = []
       eods.map do |eod|
         symbol   = eod[0..1]
@@ -40,6 +33,12 @@ module Cotcube
       result
     end
 
+    # provide a list of all eods for id/symbol or all symbols (default) for an
+    #   array of dates (default: [last_trade_date])
+    #
+    # filter by :threshold*100% share on entire volume(default) or oi
+    #
+    # return full data or just the contract name (default)
     def provide_eods(symbol: nil, # rubocop:disable Metrics/ParameterLists
                      id: nil,
                      contract: nil,
@@ -49,7 +48,7 @@ module Cotcube
                      dates: last_trade_date,
                      # set threshold to 0 to disable filtering at all.
                      # otherwise only contracts with partial of >= threshold are returned
-                     threshold: 0.1,
+                     threshold: 0.05,
                      # filter can be set to volume_part and oi_part.
                      # determines, which property is used for filtering.
                      filter: :volume_part,
@@ -57,27 +56,25 @@ module Cotcube
                      # of just the contracts matching filter and threshold
                      contracts_only: true,
                      quiet: false)
-      unless contract.nil? || (contract.is_a?(String) && [
-        3, 5
-      ].include?(contract.size))
-        raise ArgumentError,
-              "Contract '#{contract}' is bogus, should be like 'M21' or 'ESM21'"
+      unless contract.nil? || (contract.is_a?(String) && [3, 5].include?(contract.size))
+        raise ArgumentError, "Contract '#{contract}' is bogus, should be like 'M21' or 'ESM21'"
       end
 
       symbol = contract[0..1] if contract.to_s.size == 5
-      id = get_id_from(symbol: symbol, id: id, config: config)
+      sym = get_id_set(symbol: symbol, id: id, config: config) if symbol || id
       # if no id can be clarified from given arguments, return all matching contracts from all available symbols
       # raise ArgumentError, "Could not guess :id or :symbol from 'contract: #{contract}', please clarify." if id.nil?
       raise ArgumentError, ':filter must be in [:volume_part, :oi_part]' unless %i[volume_part oi_part].include? filter
 
-      ids = id.nil? ? symbols.map { |x| x[:id] } : [id]
+      # noinspection RubyScope
+      ids = sym.nil? ? symbols.map { |x| x[:id] } : [sym[:id]]
       dates = [dates] unless dates.is_a?(Array) || dates.nil?
 
       id_path_get = ->(local_id) { "#{config[:data_path]}/eods/#{local_id}" }
 
       process_date_for_id = lambda do |d, i|
-        sym        = symbols.select { |s| s[:id] == i }.first
-        symbol     = sym[:symbol]
+        # l_sym        = symbols.select { |s| s[:id] == i }.first
+        # l_symbol     = l_sym[:symbol]
         id_path    = id_path_get.call(i)
         data_file  = "#{id_path}/#{d}.csv"
         raise "No data found for requested :id (#{id_path} does not exist)" unless Dir.exist?(id_path)
