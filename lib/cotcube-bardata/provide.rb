a29a3dab4278c9db21dda32da3c47a7c23116383
Benjamin L. Tischendorf <donkeybridge@jtown.eu>
2021-01-02 00:23:14 +0000

    provide.rb: writing provide, the central accessor to actual bardata

--- lib/cotcube-bardata/provide.rb
+++ lib/cotcube-bardata/provide.rb
@@ -3,21 +3,47 @@
 module Cotcube
   module Bardata
 
-    def provide(symbol: nil, id: nil, contract:, config: init, date: Date.today - 1, type: nil, fill: :none)
+    def provide(contract:,
+                # Can be like ("2020-12-01 12:00"..."2020-12-14 11:00")
+                range: nil,              
+                symbol: nil, id: nil,
+                config: init,
+                # supported types are :quarters, :hours, :days, :rth, :dailies, :weeks, :months
+                type: :days,             
+                # supported fills are :raw, :_24x7_, :full and :rth (and custom named, if provided as file)
+                set: :full,        
+                # TODO: for future compatibility and suggestion: planning to include a function to update
+                #       with live data from broker
+                force_recent: false)
+
+      sym = get_id_set(symbol: symbol, id: id, contract: contract, config: config)
+
       case type
-      when :eod, :eods
-        provide_eods(symbol: symbol, id: id, contract: contract, date: date) 
-      when :quarters
-        print :quarters
-      when :hours
-        print :hours
-      when :daily, :dailies
-        print :dailies
+      when :quarters, :hours, :quarter, :hour
+        base = provide_quarters(contract: contract, symbol: symbol, id: id, config: config)
+        base = extended_select_for_range(range: range, base: base) if range
+        requested_set = trading_hours(symbol: sym[:symbol], set: set)
+
+        base = base.select_within(ranges: requested_set, attr: :datetime) {|x| x.to_datetime.to_sssm }
+        return base if [:quarters, :quarter].include? type
+
+        base = Cotcube::Helpers.reduce(bars: base, to: :hours){|c,b|  
+          c[:day] == b[:day] and c[:datetime].hour == b[:datetime].hour 
+        }
+
+      when :days, :weeks, :months
+        base = provide_cached contract: contract, symbol: symbol, id: id, config: config, set: set, force_recent: force_recent
+        base = extended_select_for_range(range: range, base: base) if range
+        return base if [:day, :days].include? type
+        # TODO: Missing implemetation to reduce cached days to weeks or months
+        raise "Missing implementation to reduce cached days to weeks or months"
+      when :dailies, :daily
+        base = provide_daily contract: contract, symbol: symbol, id: id, config: config
+        base = extended_select_for_range(range: range, base: base) if range
+        return base 
       else
-        puts "WARNING: Using provide without :type is for legacy support pointing to .provide_daily".light_yellow
-        provide_daily(symbol: symbol, id: id, contract: contract, config: config)
+        raise ArgumentError, "Unsupported or unknown type '#{type}' in Bardata.provide"
       end
     end
   end
-
 end
