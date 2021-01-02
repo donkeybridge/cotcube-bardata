f54929e369245ef56241012a18c068cd4f072c02
Benjamin L. Tischendorf <donkeybridge@jtown.eu>
2021-01-02 00:08:22 +0000

    applied cops to init.rb

--- lib/cotcube-bardata/init.rb
+++ lib/cotcube-bardata/init.rb
@@ -1,56 +1,53 @@
 # frozen_string_literal: true
 
 module Cotcube
+  # Missing top level documentation comment
   module Bardata
-
-
     def symbols(config: init, type: nil, symbol: nil)
       if config[:symbols_file].nil?
         SYMBOL_EXAMPLES
       else
         CSV
-          .read(config[:symbols_file], headers: %i{ id symbol ticksize power months type bcf reports name})
-          .map{|row| row.to_h }
-          .map{|row| [ :ticksize, :power, :bcf ].each {|z| row[z] = row[z].to_f}; row }
-          .reject{|row| row[:id].nil? }
-          .tap{|all| all.select!{|x| x[:type] == type} unless type.nil? }
-          .tap{|all| all.select!{|x| x[:symbol] == symbol} unless symbol.nil? }
+          .read(config[:symbols_file], headers: %i[id symbol ticksize power months type bcf reports name])
+          .map(&:to_h)
+          .map { |row| %i[ticksize power bcf].each { |z| row[z] = row[z].to_f }; row } # rubocop:disable Style/Semicolon
+          .reject { |row| row[:id].nil? }
+          .tap { |all| all.select! { |x| x[:type] == type } unless type.nil? }
+          .tap { |all| all.select! { |x| x[:symbol] == symbol } unless symbol.nil? }
       end
     end
 
     def config_prefix
-      os       = Gem::Platform.local.os
+      os = Gem::Platform.local.os
       case os
       when 'linux'
         ''
       when 'freebsd'
         '/usr/local'
       else
-        raise RuntimeError, 'unknown architecture'
+        raise 'unknown architecture'
       end
     end
 
     def config_path
-      config_prefix + '/etc/cotcube'
+      "#{config_prefix}/etc/cotcube"
     end
 
-    def init(config_file_name: 'bardata.yml', debug: false)
+    def init(config_file_name: 'bardata.yml')
       name = 'bardata'
       config_file = config_path + "/#{config_file_name}"
 
-      if File.exist?(config_file)
-        config      = YAML.load(File.read config_file).transform_keys(&:to_sym)
-      else
-        config      = {} 
-      end
+      config = if File.exist?(config_file)
+                 YAML.safe_load(File.read(config_file)).transform_keys(&:to_sym)
+               else
+                 {}
+               end
 
-      defaults = { 
-        data_path: config_prefix + '/var/cotcube/' + name,
+      defaults = {
+        data_path: "#{config_prefix}/var/cotcube/#{name}"
       }
 
-
       config = defaults.merge(config)
-
 
       # part 2 of init process: Prepare directories
 
@@ -58,25 +55,23 @@ module Cotcube
         unless Dir.exist?(directory_name)
           begin
             `mkdir -p #{directory_name}`
-            unless $?.exitstatus.zero?
+            unless $CHILD_STATUS.exitstatus.zero?
               puts "Missing permissions to create or access '#{directory_name}', please clarify manually"
               exit 1 unless defined?(IRB)
             end
-          rescue 
+          rescue StandardError
             puts "Missing permissions to create or access '#{directory_name}', please clarify manually"
             exit 1 unless defined?(IRB)
           end
         end
       end
-      ['',:daily,:quarters].each do |path| 
-        dir = "#{config[:data_path]}#{path == '' ? '' : '/'}#{path.to_s}"
+      ['', :daily, :quarters, :eods, :trading_hours, :cached].each do |path|
+        dir = "#{config[:data_path]}#{path == '' ? '' : '/'}#{path}"
         save_create_directory.call(dir)
       end
 
       # eventually return config
       config
     end
-
   end
 end
-
